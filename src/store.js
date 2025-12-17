c41aa7091fcadd4a3e872d3b9598d2d0a1106a2e
ygeng912-byte <ygeng912@gmail.com>
2025-12-17 22:51:56 +1100

    Update store.js

--- src/store.js
+++ src/store.js
@@ -1,8 +1,12 @@
 import { configureStore } from '@reduxjs/toolkit';
 import cartReducer from './CartSlice';
- const store = configureStore({
-    reducer: {
-        cart: cartReducer,
-    },
+
+// Create a Redux store using configureStore from Redux Toolkit
+const store = configureStore({
+  reducer: {
+    // 'cart' is the name of the slice in the store, and it's managed by cartReducer
+    cart: cartReducer,
+  },
 });
-export default store
+
+export default store;
